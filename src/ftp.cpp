9ca23d4259357e850653849b71a3a38a293991b5
Allan Pister <apister@port80software.com>
2012-06-07 11:35:03 +0100

    Section Add

---
+++ src/ftp.cpp
@@ -0,0 +1,542 @@
+/**
+ * @module builtin/ftp
+ * 
+ * ### Synopsis
+ * 
+ * var ftp = require('builtin/ftp');
+ * 
+ * Interface to ftplib, access to ftp functionality from SilkJS.
+ */
+
+#include "SilkJS.h"
+#include "ftplib.h"
+
+#ifdef WIN32
+static inline netbuf *ftp_HANDLE (Handle<Value>v) {
+#else
+static inline netbuf *HANDLE (Handle<Value>v) {
+#endif
+    if (v->IsNull()) {
+        ThrowException(String::New("Handle is NULL"));
+        return NULL;
+    }
+    netbuf *nControl = (netbuf *) JSEXTERN(v);
+    return nControl;
+}
+
+/**
+ * @function ftp.init
+ * 
+ * ### Synopsis
+ * 
+ * ftp.init();
+ * 
+ * Performs any required initialization for the library.
+ */
+static JSVAL ftp_init (JSARGS args) {
+    HandleScope scope;
+    FtpInit();
+    return Undefined();
+}
+
+/**
+ * @function ftp.site
+ * 
+ * ### Synopsis
+ * 
+ * var success = ftp.site(handle, cmd);
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} cmd - a string containing a 'SITE' subcommand.
+ * @return {int} success - 1 if successful or 0 on error.
+ */
+static JSVAL ftp_site (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value cmd(args[1]->ToString());
+    return scope.Close(Integer::New(FtpSite(*cmd, n)));
+}
+
+/**
+ * @function ftp.lastResponse
+ * 
+ * ### Synopsis
+ * 
+ * var response = ftp.lastResponse(handle);
+ * 
+ * Returns a pointer to the last response string sent by the server. This can be parsed by the user program to determine more information about the last request or can be displayed along with an error message.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @return {string} response - last response string or NULL if handle is not valid.
+ */
+static JSVAL ftp_lastResponse (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+    netbuf *n = ftp_HANDLE(args[0]);
+#else
+    netbuf *n = HANDLE(args[0]);
+#endif
+    return scope.Close(String::New(FtpLastResponse(n)));
+}
+
+/**
+ * @function ftp.systemType
+ * 
+ * ### Synopsis
+ * 
+ * var response = ftp.sysType(handle);
+ * 
+ * Determine system type of remote server.
+ * 
+ * Issues a SYST command to the remote system and attempts to parse the system type out of the response and returns it.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @return {string} response - system type or false if an error occurred.
+ */
+static JSVAL ftp_systemType (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    char buf[2048];
+    if (!FtpSysType(buf, 2048, n)) {
+        return scope.Close(False());
+    }
+    return scope.Close(String::New(buf));
+}
+
+/**
+ * @function ftp.size
+ * 
+ * var size = ftp.size(handle, path, mode);
+ * 
+ * Determine size of remote file.
+ * 
+ * Some servers may not support the SIZE command. 
+ * 
+ * If this request fails, the size may be available in the response to a RETR (ftp.open() with type=ftp.FILE_READ).
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path to file to get size of.
+ * @param {int} mode - either ftp.ASCII or ftp.BINARY.
+ * @return {int} size - size of remote file or false if an error occurred.
+ */
+static JSVAL ftp_size (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    char mode = (char) args[2]->IntegerValue();
+    int size;
+    if (FtpSize(*path, &size, mode, n)) {
+        return scope.Close(Integer::New(size));
+    }
+    else {
+        return False();
+    }
+}
+
+/**
+ * @function ftp.fileModified
+ * 
+ * var size = ftp.fileModified(handle, path);
+ * 
+ * Determine last modification time of remote file.  The date and time are returned as a string in the format 'YYYYMMDDHHMMSS'
+ * 
+ * Some servers may not support the MDTM command.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path to file to get modification time of.
+ * @return {int} size - size of remote file or false if an error occurred.
+ */
+static JSVAL ftp_fileModified (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    char buf[2048];
+    if (!FtpModDate(*path, buf, 2048, n)) {
+        return scope.Close(False());
+    }
+    return scope.Close(String::New(buf));
+}
+
+/**
+ * @function ftp.connect
+ * 
+ * var size = ftp.connect(host);
+ * 
+ * Connect to an FTP server.
+ * 
+ * Establishes a connection to the FTP server on the specified machine and returns a handle which can be used to initiate data transfers. 
+ * 
+ * The host name should be specified as <host> or <host>:<port>. <host> may be either a host name or ip address. <port> may be either a service name or a port number.
+ * 
+ * @param {string} host - host to connect to.
+ * @return {object} handle - handle to open connection, or false if an error occurred.
+ */
+static JSVAL ftp_connect (JSARGS args) {
+    HandleScope scope;
+    netbuf *n;
+    String::Utf8Value host(args[0]->ToString());
+    if (!FtpConnect(*host, &n)) {
+        return scope.Close(False());
+    }
+    return scope.Close(External::New(n));
+}
+
+/**
+ * @function ftp.login
+ * 
+ * var success = ftp.login(handle, username, password);
+ * 
+ * Login to remote system.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} username - username on remote system.
+ * @param {string} password - password on remote system.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_login (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value username(args[1]->ToString());
+    String::Utf8Value password(args[2]->ToString());
+    return scope.Close(Integer::New(FtpLogin(*username, *password, n)));
+}
+
+/**
+ * @function ftp.quit
+ * 
+ * ### Synopsis
+ * 
+ * ftp.quit(handle);
+ * 
+ * Closes the connection to the remote server and frees any resources associated with the connection.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ */
+static JSVAL ftp_quit (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    FtpQuit(n);
+    return Undefined();
+}
+
+/**
+ * @function ftp.pasv
+ * 
+ * var success = ftp.pasv(handle);
+ * 
+ * Set connection to PASV mode.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_pasv (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    return scope.Close(Integer::New(FtpOptions(FTPLIB_CONNMODE, FTPLIB_PASSIVE, n)));
+}
+
+/**
+ * @function ftp.active
+ * 
+ * var success = ftp.active(handle);
+ * 
+ * Set connection to ACTIVE mode.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_active (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    return scope.Close(Integer::New(FtpOptions(FTPLIB_CONNMODE, FTPLIB_PORT, n)));
+}
+
+/**
+ * @function ftp.chdir
+ * 
+ * var success = ftp.chdir(handle, path);
+ * 
+ * Sends a change working directory request to the server using the specified path.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path of new desired working directory.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_chdir (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    return scope.Close(Integer::New(FtpChdir(*path, n)));
+}
+
+/**
+ * @function ftp.mkdir
+ * 
+ * var success = ftp.mkdir(handle, path);
+ * 
+ * Sends a make directory request to the server using the specified path.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path of new desired directory.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_mkdir (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    return scope.Close(Integer::New(FtpMkdir(*path, n)));
+}
+
+/**
+ * @function ftp.rmdir
+ * 
+ * var success = ftp.rmdir(handle, path);
+ * 
+ * Sends a remove directory request to the server using the specified path.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path of remote directory to remove.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_rmdir (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    return scope.Close(Integer::New(FtpRmdir(*path, n)));
+}
+
+/**
+ * @function ftp.dir
+ * 
+ * var success = ftp.dir(handle, path, output);
+ * 
+ * Sends a LIST command to the server with the specified path.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path of remote directory to list.
+ * @param {string} output - path to local file to write listing to.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_dir (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    String::Utf8Value output(args[2]->ToString());
+    return scope.Close(Integer::New(FtpDir(*output, *path, n)));
+}
+
+/**
+ * @function ftp.getcwd
+ * 
+ * ### Synopsis
+ * 
+ * var path = ftp.getcwd(handle);
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @return {string} path - path of current remote directory.
+ */
+static JSVAL ftp_getcwd (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    char buf[2048];
+    if (!FtpPwd(buf, 2048, n)) {
+        return scope.Close(False());
+    }
+    return scope.Close(String::New(buf));
+}
+
+/**
+ * @function ftp.get
+ * 
+ * ### Synopsis
+ * 
+ * var success = ftp.get(handle, remotePath, localPath, mode);
+ * var success = ftp.get(handle, remotePath, localPath);
+ * 
+ * Retrieve a file from the remote system to a local file.  The second form will do a BINARY mode file transfer.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} remotePath - path to file on remote system to retrieve.
+ * @param {string} localPath - path to file on local file system where file will be stored..
+ * @param {int} mode - either ftp.ASCII or ftp.BINARY.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_get (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value remotePath(args[1]->ToString());
+    String::Utf8Value localPath(args[2]->ToString());
+    char mode = FTPLIB_IMAGE;
+    if (args.Length() > 3) {
+        mode = (char) args[3]->IntegerValue();
+    }
+    return scope.Close(Integer::New(FtpGet(*localPath, *remotePath, mode, n)));
+}
+
+/**
+ * @function ftp.put
+ * 
+ * ### Synopsis
+ * 
+ * var success = ftp.put(handle, localPath, remotePath, mode);
+ * var success = ftp.put(handle, localPath, remotePath);
+ * 
+ * Send a file to the remote system.  The second form will do a BINARY mode file transfer.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} localPath - path to file on local file system that will be sent to remote server.
+ * @param {string} remotePath - path to file on remote system to store.
+ * @param {int} mode - either ftp.ASCII or ftp.BINARY.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_put (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value localPath(args[1]->ToString());
+    String::Utf8Value remotePath(args[2]->ToString());
+    char mode = FTPLIB_IMAGE;
+    if (args.Length() > 3) {
+        mode = (char) args[3]->IntegerValue();
+    }
+    return scope.Close(Integer::New(FtpPut(*localPath, *remotePath, mode, n)));
+}
+
+/**
+ * @function ftp.unlink
+ * 
+ * var success = ftp.unlink(handle, path);
+ * 
+ * Requests that the server remove the specified file from the remote file system.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} path - path of remote file to remove.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_unlink (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value path(args[1]->ToString());
+    return scope.Close(Integer::New(FtpDelete(*path, n)));
+}
+
+/**
+ * @function ftp.rename
+ * 
+ * ### Synopsis
+ * 
+ * var success = ftp.rename(handle, oldPath, newPath);
+ * 
+ * Sends a rename request to the remote server.
+ * 
+ * @param {object} handle - handle returned by ftp.connect().
+ * @param {string} oldPath - path of existing remote file to be renamed.
+ * @param {string} newPath - path to rename existing file to.
+ * @return {int} success - 1 if successful, 0 if an error occurred.
+ */
+static JSVAL ftp_rename (JSARGS args) {
+    HandleScope scope;
+#ifdef WIN32
+	netbuf *n = ftp_HANDLE(args[0]);
+#else
+	netbuf *n = HANDLE(args[0]);
+#endif
+    String::Utf8Value oldPath(args[1]->ToString());
+    String::Utf8Value newPath(args[2]->ToString());
+    return scope.Close(Integer::New(FtpRename(*oldPath, *newPath, n)));
+}
+
+void init_ftp_object () {
+    HandleScope scope;
+
+    JSOBJT ftpObject = ObjectTemplate::New();
+
+    ftpObject->Set(String::New("ASCII"), Integer::New(FTPLIB_ASCII));
+    ftpObject->Set(String::New("BINARY"), Integer::New(FTPLIB_IMAGE));
+
+    ftpObject->Set(String::New("init"), FunctionTemplate::New(ftp_init));
+    ftpObject->Set(String::New("site"), FunctionTemplate::New(ftp_site));
+    ftpObject->Set(String::New("lastResponse"), FunctionTemplate::New(ftp_lastResponse));
+    ftpObject->Set(String::New("systemType"), FunctionTemplate::New(ftp_systemType));
+    ftpObject->Set(String::New("size"), FunctionTemplate::New(ftp_size));
+    ftpObject->Set(String::New("fileModified"), FunctionTemplate::New(ftp_fileModified));
+    ftpObject->Set(String::New("connect"), FunctionTemplate::New(ftp_connect));
+    ftpObject->Set(String::New("login"), FunctionTemplate::New(ftp_login));
+    ftpObject->Set(String::New("quit"), FunctionTemplate::New(ftp_quit));
+    ftpObject->Set(String::New("pasv"), FunctionTemplate::New(ftp_pasv));
+    ftpObject->Set(String::New("active"), FunctionTemplate::New(ftp_active));
+    ftpObject->Set(String::New("chdir"), FunctionTemplate::New(ftp_chdir));
+    ftpObject->Set(String::New("mkdir"), FunctionTemplate::New(ftp_mkdir));
+    ftpObject->Set(String::New("rmdir"), FunctionTemplate::New(ftp_rmdir));
+    ftpObject->Set(String::New("dir"), FunctionTemplate::New(ftp_dir));
+    ftpObject->Set(String::New("getcwd"), FunctionTemplate::New(ftp_getcwd));
+    ftpObject->Set(String::New("get"), FunctionTemplate::New(ftp_get));
+    ftpObject->Set(String::New("put"), FunctionTemplate::New(ftp_put));
+    ftpObject->Set(String::New("unlink"), FunctionTemplate::New(ftp_unlink));
+    ftpObject->Set(String::New("rename"), FunctionTemplate::New(ftp_rename));
+
+    builtinObject->Set(String::New("ftp"), ftpObject);
+}
---
+++ src/main.cpp
@@ -0,0 +1,189 @@
+/** @ignore */
+/* 
+ * File:   main.cpp
+ * Author: mschwartz
+ *
+ * Created on July 2, 2011, 2:27 PM
+ */
+
+#include "SilkJS.h"
+#include <v8-debug.h>
+//#define DEBUGME
+//#define NOFORK
+
+char *readFile (const char *fn) {
+#ifdef WIN32
+	FILE* fd = fopen(fn, "r");
+	if (fd == NULL) return NULL;
+	fseek(fd, 0, SEEK_END);
+	long size = ftell(fd);
+	fseek(fd, 0, SEEK_SET);
+    char *file = new char[size + 1];
+    size = fread(file, sizeof(char), size, fd);
+    file[size] = '\0';
+    fclose(fd);
+#else
+    int fd = open(fn, O_RDONLY);
+    if (fd < 0) {
+        return NULL;
+    }
+    long size = lseek(fd, 0, 2);
+    lseek(fd, 0, 0);
+    char *file = new char[size + 1];
+    size = read(fd, file, size);
+    file[size] = '\0';
+    close(fd);
+#endif
+    return file;
+}
+
+void AnsiSignalHandler (int sig) {
+    signal(sig, AnsiSignalHandler);
+    printf("Caught Signal %d for process: %ld\n", sig, (size_t) getpid());
+    exit(11);
+}
+
+//int serverSocket;
+
+Persistent<Context> context;
+Persistent<Script> mainScript;
+Persistent<Function> mainFunc;
+
+void debugger () {
+    Context::Scope scope(context);
+    Debug::ProcessDebugMessages();
+}
+
+// Extracts a C string from a V8 Utf8Value.
+
+const char* ToCString (const v8::String::Utf8Value& value) {
+    return *value ? *value : "<string conversion failed>";
+}
+
+void ReportException (v8::TryCatch* try_catch) {
+    v8::HandleScope handle_scope;
+    v8::String::Utf8Value exception(try_catch->Exception());
+    const char* exception_string = ToCString(exception);
+    v8::Handle<v8::Message> message = try_catch->Message();
+    if (message.IsEmpty()) {
+        // V8 didn't provide any extra information about this error; just
+        // print the exception.
+        printf("%s\n", exception_string);
+    }
+    else {
+        // Print (filename):(line number): (message).
+        v8::String::Utf8Value filename(message->GetScriptResourceName());
+        const char* filename_string = ToCString(filename);
+        int linenum = message->GetLineNumber();
+        printf("%s:%i: %s\n", filename_string, linenum, exception_string);
+        // Print line of source code.
+        v8::String::Utf8Value sourceline(message->GetSourceLine());
+        const char* sourceline_string = ToCString(sourceline);
+        printf("%s\n", sourceline_string);
+        // Print wavy underline (GetUnderline is deprecated).
+        int start = message->GetStartColumn();
+        for (int i = 0; i < start; i++) {
+            printf(" ");
+        }
+        int end = message->GetEndColumn();
+        for (int i = start; i < end; i++) {
+            printf("^");
+        }
+        printf("\n");
+        v8::String::Utf8Value stack_trace(try_catch->StackTrace());
+        if (stack_trace.length() > 0) {
+            const char* stack_trace_string = ToCString(stack_trace);
+            printf("%s\n", stack_trace_string);
+        }
+        else {
+            printf("no stack trace available\n");
+        }
+    }
+}
+
+int main (int argc, char** argv) {
+    signal(SIGSEGV, AnsiSignalHandler);
+    //    printf("SILK V0.1\n");
+    char *startup;
+    const char *progName;
+
+    if (argc < 2) {
+        startup = readFile("/usr/share/silkjs/builtin/interpreter.js");
+        progName = "interpreter";
+    }
+    else {
+        startup = readFile(argv[1]);
+        progName = argv[1];
+    }
+    if (!startup) {
+        printf("%s not found\n", argv[1]);
+        exit(1);
+    }
+    if (startup[0] == '#' && startup[1] == '!') {
+        startup[0] = startup[1] = '/';
+    }
+    {
+        //		Isolate *isolate = Isolate::New();
+        //		isolate->Enter();
+        //		Locker lock(isolate);
+        Locker locker;
+        HandleScope scope;
+
+        init_global_object();
+
+        V8::SetCaptureStackTraceForUncaughtExceptions(true, 50); // , StackTrace::kDetailed);
+        context = Context::New(NULL, globalObject);
+
+        Context::Scope context_scope(context);
+        {
+            Locker locker;
+            //			Debug::SetDebugMessageDispatchHandler(debugger, true);
+            //			Debug::EnableAgent("silkjs", 5858);
+
+            Handle<Script>init = Script::New(String::New("global=this; module = {}; include('builtin/all.js');"), String::New("builtin"));
+
+            init->Run();
+
+            mainScript = Persistent<Script>::New(Script::Compile(String::New(startup), String::New(progName)));
+            V8::SetCaptureStackTraceForUncaughtExceptions(true, 50, StackTrace::kDetailed);
+            TryCatch tryCatch;
+
+            Handle<Value>v = mainScript->Run();
+            if (v.IsEmpty()) 
+			{
+                ReportException(&tryCatch);
+                exit(1);
+            }
+
+            Handle<String> process_name = String::New("main");
+            Handle<Value> process_val = context->Global()->Get(process_name);
+
+            if (!process_val.IsEmpty() && process_val->IsFunction()) 
+			{
+                Handle<Function> process_fun = Handle<Function>::Cast(process_val);
+                mainFunc = Persistent<Function>::New(process_fun);
+                int ac = argc - 2;
+                if (ac < 0) {
+                    ac = 0;
+                }
+
+#ifdef WIN32
+				// Just did this so i can compile, this will need to change.
+				Handle<Value>av[15];		
+#else
+				Handle<Value>av[ac];
+#endif
+                for (int i = 2; i < argc; i++) {
+                    av[i - 2] = String::New(argv[i]);
+                }
+                v = mainFunc->Call(context->Global(), ac, av);
+                if (v.IsEmpty()) {
+                    ReportException(&tryCatch);
+                    exit(1);
+                }
+            }
+        }
+        context.Dispose();
+    }
+}
+
